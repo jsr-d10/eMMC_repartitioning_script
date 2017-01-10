0430827d70ac6ec9babb0414c9513786c630d7c6
S-trace <S-trace@list.ru>
2017-01-10 20:16:52 +0300

    [PACKER] Check GPT md5sum while packing ZIPs

--- build_zips.sh
+++ build_zips.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+E_GPTMD5MISMATCH=3
+E_NOTEMPLATE=4
 templates_dir="GPTs"
 rm ./*.zip
 for dir in "$templates_dir"/*; do
@@ -6,5 +8,15 @@ for dir in "$templates_dir"/*; do
   echo "Processing $template"
   zip -r "$template.zip" META-INF fstab
   ZIP="$(readlink -f "$template.zip")"
-  (cd "$dir" || exit 2; zip "$ZIP" ./*)
+  (
+    cd "$dir" || exit "$E_NOTEMPLATE"
+    if md5sum --check < new.gpt.md5sum; then
+      zip "$ZIP" ./*
+    else
+      rm "$ZIP"
+      echo "$template: GPT MD5 mismatch!"
+      exit "$E_GPTMD5MISMATCH"
+    fi
+  ) || exit "$?"
+  echo "\n"
 done
